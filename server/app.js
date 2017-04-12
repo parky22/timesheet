a4818e5a7a4220c0e9c04aaf69fced3c9c5fff0e
Yoo-Nah Park <ypark2270@gmail.com>
2017-04-12 15:19:11 -0400

    set up express server

--- server/app.js
+++ server/app.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, './public')));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use('/employees', require('./routes/employees-route'));
+
 app.use((err, req, res, next) => {
-  res.status(err.status || 500).send(err.message || "something went wrong");
+  res.status(err.status || 500).send(err.message || 'something went wrong');
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
+
+module.exports = app;
